424aa990601be4e2b04c60ff45bcf19788a2cae8
Richard Nienaber <rjnienaber@gmail.com>
2020-11-16 00:49:54 +0000

    wip initial setup

---
+++ test/spec.js
@@ -0,0 +1,47 @@
+const Application = require('spectron').Application
+const assert = require('assert')
+const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
+const path = require('path')
+
+describe('Application launch', function () {
+  self = this;
+  this.timeout(20000)
+
+  beforeEach(function () {
+    self.app = new Application({
+      // Your electron path can be any binary
+      // i.e for OSX an example path could be '/Applications/MyApp.app/Contents/MacOS/MyApp'
+      // But for the sake of the example we fetch it from our node_modules.
+      path: electronPath,
+
+      // Assuming you have the following directory structure
+
+      //  |__ my project
+      //     |__ ...
+      //     |__ main.js
+      //     |__ package.json
+      //     |__ index.html
+      //     |__ ...
+      //     |__ test
+      //        |__ spec.js  <- You are here! ~ Well you should be.
+
+      // The following line tells spectron to look and use the main.js file
+      // and the package.json located 1 level above.
+      args: ['--no-sandbox', path.join(__dirname, '..')]
+    })
+    return self.app.start()
+  })
+
+  afterEach(function () {
+    if (self.app && self.app.isRunning()) {
+      return self.app.stop()
+    }
+  })
+
+  it('shows an initial window', async () => {
+    const count = await self.app.client.getWindowCount()
+    assert.equal(count, 2)
+  })
+
+  // TODO: https://circleci.com/blog/electron-testing/
+})
